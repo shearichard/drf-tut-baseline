73eda67e1e5de4539e61735a1aa2bec5433300bf
Richard Shea <rshea@thecubagroup.com>
2023-12-08 08:59:26 +1300

    DRF Tutorial - at the end of page 5

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,7 +4,25 @@ from django.contrib.auth.models import User
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class UserSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
+
+
+class UserSerializerFirstVersion(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
 
     class Meta:
@@ -12,8 +30,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'snippets']
 
 
-
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializerSecondVersion(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
 
